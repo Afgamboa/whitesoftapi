666749f9a6ee7ef6fa949c4909dca13d032888c8
Andresfg24 <andresfg2124@gmail.com>
2023-05-30 12:18:31 -0500

    port

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const port = 3001
 
 app.listen(port, () => {
-    console.log("Server running on port", port)
+    console.log("Server running on port", 3000)
 })
