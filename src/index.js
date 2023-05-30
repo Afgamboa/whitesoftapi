83b37b8ed518b94f717c4b09028a657d1a978b07
Andresfg24 <andresfg2124@gmail.com>
2023-05-30 13:02:35 -0500

    route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 3001
 
+app.use('/', () => {
+    console.log("hola mundo")
+})
+
 app.listen(port, () => {
     console.log("Server running on port", port)
 })
